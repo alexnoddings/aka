d27f71d77e4a13d0cb7bb38a082c8221649c36eb
Alex Noddings <alexnoddings@users.noreply.github.com>
2026-04-04 19:37:01 +0100

    feat: add disabling/enabling links

---
+++ admin/src/routes/links/[id]/disable/+page.server.ts
@@ -0,0 +1,60 @@
+import type { Actions, PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import { getExistingSlugId, getForm, idExists, validateForm } from '../../validation.ts';
+
+export const load: PageServerLoad = async ({ params, platform }) => {
+	const db = platform?.env.aka;
+	if (!db) {
+		throw Error('database unavailable');
+	}
+
+	const id = params.id;
+	const link = await db
+		.prepare(`SELECT id, slug, notes FROM links WHERE id=?`)
+		.bind(id)
+		.first<Link>();
+
+	return { link: link };
+};
+
+type Link = {
+	id: string;
+	slug: string;
+	notes: string;
+};
+
+export const actions = {
+	default: async ({ request, platform, params }) => {
+		const id = params.id;
+
+		const db = platform?.env.aka;
+		if (!db) {
+			throw Error('database unavailable');
+		}
+
+		const data = await request.formData();
+		const form = getForm(data);
+		if (!(await idExists(id, db))) {
+			return fail(404, {
+				...form,
+				errors: {
+					id: 'Link not found'
+				}
+			});
+		}
+
+		const now = new Date().toISOString();
+		await db
+			.prepare(
+				`
+				UPDATE links
+				SET is_active  = 0,
+				    updated_at = ?2
+				WHERE id = ?1`
+			)
+			.bind(id, now)
+			.run();
+
+		return redirect(302, `/links`);
+	}
+} satisfies Actions;
---
+++ admin/src/routes/links/[id]/disable/+page.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	import type { ActionData, PageData } from './$types';
+	import OcticonComment from '$/icons/OcticonComment.svelte';
+
+	interface Props {
+		data: PageData;
+		form: ActionData;
+	}
+
+	let { data, form = $bindable() }: Props = $props();
+</script>
+
+<svelte:head>
+	<title>Disable link | aka</title>
+</svelte:head>
+
+<div class="container-sm">
+	<div class="page-header">
+		<div>
+			<h1 class="page-title">Disable link</h1>
+		</div>
+	</div>
+
+	{#if !data.link}
+		<div class="surface surface--content">
+			<p class="error">Link not found</p>
+			<a href="/links" class="link link--info">Back to links</a>
+		</div>
+	{:else}
+		<form method="post">
+			<div class="card">
+				<div class="card__header">
+					{#if data.link.slug.length === 0}
+						<div class="card__header__title slug slug-default">default</div>
+					{:else}
+						<div class="card__header__title slug">
+							{data.link.slug}
+						</div>
+					{/if}
+				</div>
+				<div class="card__body card__body--content">
+					<p>Disabled links won't redirect users.</p>
+					<p>They are still be preserved for analytics and can be re-enabled later.</p>
+				</div>
+				{#if !!data.link.notes && data.link.notes.length > 0}
+					<div class="card__footer">
+						<OcticonComment />
+						{data.link.notes}
+					</div>
+				{/if}
+			</div>
+
+			{#if !!form?.errors.id}
+				<div>
+					{form?.errors.id}
+				</div>
+			{/if}
+
+			<div class="button-group">
+				<button class="button button--danger button-lg" type="submit"> Disable </button>
+				<a href="/links/{data.link.id}" class="button button--text button-lg"> Cancel </a>
+			</div>
+		</form>
+	{/if}
+</div>
+
+<style>
+	.slug {
+		font-size: 1.5rem;
+		line-height: 1.2;
+		padding-inline: var(--spacing);
+
+		&.slug-default {
+			font-style: italic;
+			color: var(--color-text-muted);
+		}
+	}
+
+	.button-group {
+		margin-top: calc(var(--spacing) * 4);
+		padding-inline: calc(var(--spacing) * 3);
+	}
+</style>
